202401bb5526cffebf52cc3df03630b087337645
Zheng YU <zyuu0045@gmail.com>
2023-10-15 01:04:58 +1100

    Restaurant Tab

--- A3.R
+++ A3.R
@@ -1,6 +1,25 @@
 library('shiny')
 library('ggplot2')
 library('ggiraph')
+library('treemapify')
+library('highcharter')
+library('shinyjs')
+
+source('source/tableau-in-shiny-v1.0.R')
+
+# Get data
+restaurant_data <- read.csv('dataset/restaurant_dataset.csv')
+
+names(restaurant_data)[names(restaurant_data) == "google_maps_rating"] <- "Rating on Google Maps(0~5)"
+names(restaurant_data)[names(restaurant_data) == "rating"] <- "Rating on Tripadvisor(0~5)"
+
+restaurant_data$tripadvisor_link <- sprintf("window.open('%s')", restaurant_data$tripadvisor_link)
+
+# Generate variables for horizontal and vertical coordinates
+x_y_vars <- setdiff(names(restaurant_data), c("name", "tripadvisor_link", "description", "cuisines", "mon", "tue", "wed", "thu", "fri", "sat", "sun"))
+
+split_cuisines <- unlist(strsplit(restaurant_data[["cuisines"]], ", "))
+unique_cuisines_list <- c("All", unique(split_cuisines))
 
 home_tab <- tabPanel(
   title='Home',
@@ -20,12 +39,30 @@ senic_spot_tab <- tabPanel(
 
 restaurant_tab <- tabPanel(
   title='Restaurant',
-  h2('h2')
-)
-
-restaurant_tab <- tabPanel(
-  title='Restaurant',
-  h2('h2')
+  h2('h2'),
+  fluidPage(
+    fluidRow(
+      column(3,
+             tableauPublicViz(
+               id = 'tableauViz',
+               url = 'https://public.tableau.com/views/restaurant_word_cloud/Sheet1?:language=zh-CN&publish=yes&:display_count=n&:origin=viz_share_link',
+               height = '300px'
+             ),
+      ),
+      column(3,
+             h3("Configuration", style = "font-weight: bold;"),
+             # Allow the user to customize the horizontal and vertical coordinates according  to the information they want to get.
+             # Among the values that can be used as coordinates are release year, runtime, IMDb rating, Meta score, number of votes and box office.
+             # Kmeans example: https://shiny.posit.co/r/gallery/start-simple/kmeans-example/
+             selectInput("xcol", "X-axis variable", x_y_vars),
+             selectInput('selectedCuisine', 'Cuisines', unique_cuisines_list, selected="All", selectize=TRUE)
+             
+      ),
+      mainPanel(
+        highchartOutput('plot_restaurant')
+      )
+    )
+  )
 )
 
 weather_tab <- tabPanel(
@@ -34,8 +71,9 @@ weather_tab <- tabPanel(
 )
 
 ui <- navbarPage(
+  header = setUpTableauInShiny(),
   id='home_page', 
-  title='IMDb Top 1000 Movies',
+  title='Welcome to Melbourne',
   home_tab,
   senic_spot_tab,
   restaurant_tab,
@@ -57,15 +95,73 @@ server <- function(input, output, session) {
   observeEvent(input$btn_weather, {
     updateNavbarPage(session, "home_page", selected = "Weather")
   })
+  
   output$plot_home <- renderUI({
-    
     
   })
   
+  selected_restaurant_data <- reactive({
+    filtered_data <- restaurant_data[
+      
+    ]
+    
+    # If a specific cuisine is chosen, filter the rows accordingly
+    #if (!is.null(input$selectedCuisine)) {
+     # if(input$selectedCuisine == "All"){
+      #  filtered_data <- restaurant_data
+      #}else{
+       # filtered_data <- filtered_data[grepl(input$selectedCuisine, filtered_data$cuisines), ]
+      #}
+    #}
+    if (!is.null(input$tableauViz_mark_selection_changed$all_splited_cuisines[1])) {
+      filtered_data <- filtered_data[grepl(input$tableauViz_mark_selection_changed$all_splited_cuisines[1], filtered_data$cuisines), ]
+    }else{
+      filtered_data <- restaurant_data
+    }
+    
+    # filter relevant columns
+    filtered_data[, c("name", "Rating on Google Maps(0~5)", "Rating on Tripadvisor(0~5)", "tripadvisor_link", input$xcol, "cuisines")]
+  })
+  
+  output$plot_restaurant <- renderHighchart({
+    
+    
+    name_var <- names(selected_restaurant_data())[1]
+    google_maps_rating_var <- names(selected_restaurant_data())[2]
+    rating_var <- names(selected_restaurant_data())[3]
+    tripadvisor_link_var <- names(selected_restaurant_data())[4]
+    x_var <- names(selected_restaurant_data())[5]
+    cuisines_var <- names(selected_restaurant_data())[6]
+    
+    # Prepare the links in a format that's executable by JavaScript
+    #selected_restaurant_data()[[tripadvisor_link_var]] <- sprintf("window.open('%s')", selected_restaurant_data()[[tripadvisor_link_var]])
+    hchart(selected_restaurant_data(), "treemap", hcaes(
+      name = !!sym(name_var), 
+      value = !!sym(x_var), 
+      color = !!sym(x_var),
+      events.click = !!sym(tripadvisor_link_var)
+    )) %>% 
+      hc_tooltip(pointFormat = paste(
+        "{point.name}<br>",
+        "Tripadvisor Rating: {point.value}<br>",
+        sprintf("Google Maps Rating: {point.%s}<br>", google_maps_rating_var),
+        sprintf("Cuisines: {point.%s}<br>", cuisines_var),
+        "Click for more details on Tripadvisor"
+      )) %>%
+      hc_plotOptions(
+        series = list(
+          point = list(
+            events = list(
+              click = JS("function() { eval(this.options.tripadvisor_link); }")
+            )
+          )
+        )
+      )
+  })
   
 }
 
 #############
 # Run Shiny #
 #############
-shinyApp(ui, server)
+shinyApp(ui, server, options = list(launch.browser=TRUE))
